f7a5c161b9470dc4e16969904ff3aac13039d7ea
dev <dev@example.com>
2023-02-20 18:46:30 +0530

    payment gateway learn

---
+++ app/Http/Controllers/StripeController.php
@@ -0,0 +1,93 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Stripe\Exception\CardException;
+use Stripe\StripeClient;
+
+
+class StripeController extends Controller
+{
+    private $stripe;
+    public function __construct()
+    {
+        $this->stripe = new StripeClient(env('STRIPE_SECRET'));
+    }
+    public $STRIPE_SECRET = 'pk_test_GCNQjUbwQtrHqqQDEaeZskk1';
+
+
+    public function index()
+    {
+        return view('payment');
+    }
+
+    public function payment(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'fullName' => 'required',
+            'cardNumber' => 'required',
+            'month' => 'required',
+            'year' => 'required',
+            'cvv' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            $request->session()->flash('danger', $validator->errors()->first());
+            return response()->redirectTo('/');
+        }
+
+        $token = $this->createToken($request);
+        if (!empty($token['error'])) {
+            $request->session()->flash('danger', $token['error']);
+            return response()->redirectTo('/');
+        }
+        if (empty($token['id'])) {
+            $request->session()->flash('danger', 'Payment failed.');
+            return response()->redirectTo('/');
+        }
+
+        $charge = $this->createCharge($token['id'], 2000);
+        if (!empty($charge) && $charge['status'] == 'succeeded') {
+            $request->session()->flash('success', 'Payment completed.');
+        } else {
+            $request->session()->flash('danger', 'Payment failed.');
+        }
+        return response()->redirectTo('/stripe');
+    }
+
+    private function createToken($cardData)
+    {
+        $token = null;
+        try {
+            $token = $this->stripe->tokens->create([
+                'card' => [
+                    'number' => $cardData['cardNumber'],
+                    'exp_month' => $cardData['month'],
+                    'exp_year' => $cardData['year'],
+                    'cvc' => $cardData['cvv']
+                ]
+            ]);
+        } catch (CardException $e) {
+            $token['error'] = $e->getError()->message;
+        } catch (Exception $e) {
+            $token['error'] = $e->getMessage();
+        }
+        return $token;
+    }
+
+    private function createCharge($tokenId, $amount)
+    {
+        $charge = null;
+        try {
+            $charge = $this->stripe->charges->create([
+                'amount' => $amount,
+                'currency' => 'usd',
+                'source' => $tokenId,
+                'description' => 'My first payment'
+            ]);
+        } catch (Exception $e) {
+            $charge['error'] = $e->getMessage();
+        }
+        return $charge;
+    }
+}
